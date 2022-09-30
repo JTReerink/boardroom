14a290a4c6fbe2693df2308148b129afaa5a1de8
JTReerink <jareerink@gmail.com>
2022-09-30 14:53:41 +0200

    Setup TaskData

---
+++ src/Components/data/TaskData.js
@@ -0,0 +1,14 @@
+const Task = [
+    {
+        name: "item1",
+        done: false,
+        id: 1,
+    },
+    {
+        name: "item2",
+        done: false,
+        id: 2,
+    }
+];
+
+export default {Task};
