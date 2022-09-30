a8ea6aac5617afd3bbe9b5b48ca75e58140950e2
JTReerink <jareerink@gmail.com>
2022-09-30 14:55:02 +0200

    Fix Tasklist

--- src/Components/Takslist/Tasklist.js
+++ src/Components/Takslist/Tasklist.js
@@ -1,15 +1,35 @@
-import Task from "../Task/Task";
+import React from "react";
+import TaskItem from "../TaskItem/TaskItem";
+import taskObject from "../data/TaskData";
 import "./Tasklist.css";
 
-const List = () => {
-    return(
-        <article className="tasklist">
-            <h1>Ik ben een list</h1>
-            <ul className="tasklist__list">
-                <Task />
-            </ul>
-        </article>
-    )
+class List extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ tasks: taskObject.Task})
+    }
+    
+    render(){
+        let task = this.state.tasks.map(task => {
+            return <TaskItem key={task.id} name={task.name} done={task.done} />
+        })
+        return(
+            <article className="tasklist">
+                <h1 className="tasklist__title">Ik ben een list</h1>
+                <ul className="tasklist__list">
+                    {task}
+                </ul>
+            </article>
+        )
+
+    }
 };
 
 export default List;
